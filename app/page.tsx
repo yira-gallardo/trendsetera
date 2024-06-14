dba98101cf714bbbeeaf3f2643085df07238e33d
Vannelo <work@vannelo.com>
2024-06-14 11:33:10 -0600

    Add video

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,14 @@
-"use client";
-
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="bg-black pt-20 trend-1 min-h-screen">
-      <Image
-        src="/img/desktop.png"
-        alt="hero"
-        layout="fill"
-        objectFit="cover"
-        quality={100}
-        className="hidden md:block"
-      />
-      <Image
-        src="/img/mobile.png"
-        alt="hero"
-        layout="fill"
-        objectFit="cover"
-        quality={100}
-        className="block md:hidden"
-      />
-      {/* <div className="overflow-hidden">
-        <video loop className="w-full" autoPlay muted>
-          <source src="/img/video.mp4" type="video/mp4" />
-        </video>
-      </div> */}
+      <div className="aspect-w-16 aspect-h-9">
+        <iframe
+          src="https://www.youtube.com/embed/8Xw2Dt2UYrY?autoplay=1&mute=1"
+          allow="autoplay; encrypted-media"
+          allowFullScreen
+          className="absolute top-0 left-0 w-full h-full"
+        ></iframe>
+      </div>
     </main>
   );
 }
